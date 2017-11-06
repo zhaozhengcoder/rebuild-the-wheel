3b6fa140e36f8366e410670977c950a9d7048663
赵政 <www.a821400729@qq.com>
2017-11-06 23:07:58 +0800

    httpd

---
+++ tinyhttpd/rebuild_mytinyhttpd/httpd.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <ctype.h>
+#include <strings.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <pthread.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <stdint.h>
